9e22949cd2e3f7ea8f9f19724bb20a8462ee754f
patrick-7space <ortizgarciapatrick7@gmail.com>
2026-05-08 22:25:17 -0500

    Add files via upload

---
+++ GUIA1_EJE1/src/guia1_eje1/ClaseAlumnos.java
@@ -0,0 +1,35 @@
+
+package guia1_eje1;
+public class ClaseAlumnos {
+    //Atributos 
+    private String dni;
+    private String nombres;
+    private String apellidos;
+    //CONSTRUCTOR 
+    public ClaseAlumnos(){
+        dni="12345678";
+        nombres= "patrick antoni";
+        apellidos="Ortiz Garcia";
+    }
+    //METODOS ESCRITURA
+    public void setDni (String xdni){
+        dni=xdni;
+    }
+    public void setNombres (String xnombres){
+        nombres=xnombres;
+    }
+    public void setApellidos (String xapellidos){
+        apellidos=xapellidos ;
+    }
+    //METODOS LECTURA 
+    public String getDni(){
+        return dni;
+    }
+    public String getNombres (){
+        return nombres;
+    }
+     public String getApellidos (){
+        return apellidos;
+    }
+}
+
